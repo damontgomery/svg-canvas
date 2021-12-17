0083613defdc9cbd4c8c7fbde8576ef159ec2756
Daniel Montgomery <damontgomery@gmail.com>
2021-12-17 13:15:02 -0600

    Add documentation link.

--- src/Path.tsx
+++ src/Path.tsx
@@ -1,11 +1,13 @@
 import { PathShape } from './shapes'
 import { rgba } from './utilities/color'
 
+// See https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Paths for a guide on what `path` should look like.
+
 function Path(props: { shape: PathShape }) {
   const d = props.shape.coordinates.length > 0
     ? `M ${props.shape.coordinates
       .map(coordinate => `${coordinate.x} ${coordinate.y}`)
-      .join(' L')
+      .join(' L ')
     }`
     : 'M 0 0'
 
