bc28ee31e7ede854c8f0173d66b7b587c8d62057
Daniel Montgomery <damontgomery@gmail.com>
2021-12-17 13:05:09 -0600

    Use random color for default path.

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -5,7 +5,21 @@ import { PathShape, Shape, ShapeName, shapesToComponents } from './shapes'
 function Canvas() {
   const canvasRef = useRef<HTMLDivElement>(null)
 
-  const [shapes, setShapes] = useState([new PathShape])
+  const defaultShape = new PathShape;
+  // Random color for fun.
+
+  function randomColor() {
+    return Math.round(Math.random() * 255)
+  }
+
+  defaultShape.stroke = {
+    red: randomColor(),
+    green: randomColor(),
+    blue: randomColor(),
+    opacity: 1,
+  }
+
+  const [shapes, setShapes] = useState([defaultShape])
 
   function handleClick(event: MouseEvent) {
     let newShapes = Array.from(shapes)
@@ -24,10 +38,10 @@ function Canvas() {
 
   const shapeComponents = shapes.map((shape, index) => {
     const shapeName = (shape.constructor.name as ShapeName)
-  
+
     const ShapeComponent = shapesToComponents.get(shapeName);
 
-    return ShapeComponent !== undefined 
+    return ShapeComponent !== undefined
       ? (
         <ShapeComponent
           key={`shape-${index}`}
